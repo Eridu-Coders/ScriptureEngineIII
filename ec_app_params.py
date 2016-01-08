bf6116ac9389aca70140a1f1c91f580664acaf89
fi11222 <nicolas.reimen@gmail.com>
2016-01-08 20:53:20 +0100

    prod config

--- ec_app_params.py
+++ ec_app_params.py
@@ -8,8 +8,8 @@ g_appVersion = '3.0.9'
 g_appTitle = 'Followup on QuranSE 1.0 and 2.0'
 
 # HTTP server parameters
-g_appDomain = 'localhost'
-# g_appDomain = 'scripture-search.org'
+# g_appDomain = 'localhost'
+g_appDomain = 'scripture-search.org'
 g_httpPort = 8000
 # in days
 g_cookiePersistence = 365*100
@@ -18,8 +18,8 @@ g_cookiePersistence = 365*100
 g_sessionName = 'ECTerminalID_{0}'.format(g_appName)
 
 # Database parameters
-g_dbServer = '192.168.0.51'
-#g_dbServer = 'localhost'
+# g_dbServer = '192.168.0.51'
+g_dbServer = 'localhost'
 g_dbDatabase = 'ScriptureEngineIII'
 g_dbUser = 'root'
 g_dbPassword = 'murugan!'
