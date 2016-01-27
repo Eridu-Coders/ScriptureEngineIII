2481b9117dd9756e0c54502442d22914fe647aeb
fi11222 <nicolas.reimen@gmail.com>
2016-01-27 07:09:17 +0100

    lexicon cleanup

--- se3_lexicon.py
+++ se3_lexicon.py
@@ -11,15 +11,22 @@ if ec_app_params.g_verboseModeOn:
 if ec_app_params.g_debugModeOn:
     g_loggerLexicon.setLevel(logging.DEBUG)
 
+# -------------------------- Logger ------------------------------------------------------------------------------------
+# Arabic alphabet : useless but nice
 g_alphabet = 'أ ب ت ث ج ح خ د ذ ر ز س ش ص ض ط ظ ع غ ف ق ك ل م ن ه و ي'
 
 
+# Arabic lexicon
 def lexicon_arabic(p_previousContext, p_context, p_dbConnectionPool):
 
     l_dbConnection = p_dbConnectionPool.getConnection()
 
-    l_response = '<div class="LexOuter"><div class="LexLeft">\n'
+    # Page structure : an outer div with an inner floating table (display: inline-block)
+    # with 2 columns
+    # each column contain inline-block tables for each letter of the Arabic alphabet
+    l_response = '<div class="LexOuter">\n<table class="LexInner"><tr><td class="LexLeft">\n'
 
+    # All Arabic roots in alphabetical order
     l_query = """
         SELECT
             ST_GROUND
@@ -37,41 +44,50 @@ def lexicon_arabic(p_previousContext, p_context, p_dbConnectionPool):
         l_letterCount = 0
         for l_ground, l_translit, l_idRoot in l_cursor:
             if l_curInitial != l_ground[0]:
+                # letter break
                 if l_curInitial != '':
+                    # if it is not the first one --> end of table and br so that the next table will be
+                    # underneath it. Otherwise, since the tables are inline-block, the next one will be to the right
                     l_response += '</table><br/>'
 
                 g_loggerLexicon.debug('l_letterCount {0}'.format(l_letterCount))
                 if l_letterCount == 15:
-                    for i in range(58):
-                        l_response += '<p>&nbsp;</p>\n'
-                    l_response += '</div><div class="LexRight">\n'
+                    # column break at 15th letter: this is closest to 1/2
+                    l_response += '</td><td class="LexRight">\n'
 
                 l_letterCount += 1
 
+                # begin new table
                 l_response += '<table class="ArabLex">\n' + \
                               '<tr><td colspan="2" class="LexLetter">{0}</td></tr>\n'.format(l_ground[0])
                 l_curInitial = l_ground[0]
 
+            # Link to root
             l_rootLink = '<a href="" class="GoRoot" p_idroot="{1}">{0}</a>'.format(l_ground, l_idRoot.decode('utf-8'))
 
+            # left cell --> root
             l_response += '<tr><td class="LexGround">{0}</td>\n'.format(l_rootLink)
+            # right cell --> translit
             l_response += '<td class="LexTranslit">{0}</td></tr>\n'.format(l_translit)
 
         l_cursor.close()
     except Exception as l_exception:
         g_loggerLexicon.warning('Something went wrong {0}'.format(l_exception.args))
 
-    l_response += '</table></div></div>\n'
+    # end
+    l_response += '</table></td></tr></table></div>\n'
 
     p_dbConnectionPool.releaseConnection(l_dbConnection)
 
     return l_response, p_context, ec_app_params.g_appTitle
 
 
+# hebrew lexicon : same principle
+# except that here there are some translations in addition to transliterations
 def lexicon_hebrew(p_previousContext, p_context, p_dbConnectionPool):
     l_dbConnection = p_dbConnectionPool.getConnection()
 
-    l_response = '<div class="LexOuter"><div class="LexLeft">\n'
+    l_response = '<div class="LexOuter">\n<table class="LexInner"><tr><td class="LexLeft">\n'
 
     l_query = """
         SELECT
@@ -96,9 +112,7 @@ def lexicon_hebrew(p_previousContext, p_context, p_dbConnectionPool):
 
                 g_loggerLexicon.debug('l_letterCount {0}'.format(l_letterCount))
                 if l_letterCount == 12:
-                    for i in range(171):
-                        l_response += '<p>&nbsp;</p>\n'
-                    l_response += '</div><div class="LexRight">\n'
+                    l_response += '</td><td class="LexRight">\n'
 
                 l_letterCount += 1
 
@@ -108,20 +122,24 @@ def lexicon_hebrew(p_previousContext, p_context, p_dbConnectionPool):
 
             l_rootLink = '<a href="" class="GoRoot" p_idroot="{1}">{0}</a>'.format(l_ground, l_idRoot.decode('utf-8'))
 
+            # first cell --> root
             l_response += '<tr><td class="LexGround">{0}</td>\n'.format(l_rootLink)
+            # second cell --> transliteration
             l_response += '<td class="LexTranslit">{0}</td>\n'.format(l_translit)
+            # third cell --> translation
             l_response += '<td class="LexDef">{0}</td></tr>\n'.format(l_def)
 
         l_cursor.close()
     except Exception as l_exception:
         g_loggerLexicon.warning('Something went wrong {0}'.format(l_exception.args))
 
-    l_response += '</table></div></div>\n'
+    l_response += '</table></td></tr></table></div>\n'
 
     p_dbConnectionPool.releaseConnection(l_dbConnection)
 
     return l_response, p_context, ec_app_params.g_appTitle
 
+# greek letter dictionary to remove diacritics
 g_greekLetterTranslate = {
     'Α': 'Α',
     'Β': 'Β',
@@ -228,10 +246,12 @@ g_greekLetterTranslate = {
 }
 
 
+# greek lexicon
+# same general principle except that here there are no roots and that the lexicon entries are therefore words
 def lexicon_greek(p_previousContext, p_context, p_dbConnectionPool):
     l_dbConnection = p_dbConnectionPool.getConnection()
 
-    l_response = '<div class="LexOuter"><div class="LexLeft">\n'
+    l_response = '<div class="LexOuter">\n<table class="LexInner"><tr><td class="LexLeft">\n'
 
     l_query = """
         SELECT
@@ -252,16 +272,17 @@ def lexicon_greek(p_previousContext, p_context, p_dbConnectionPool):
         l_curInitial = ''
         l_letterCount = 0
         for l_ground, l_translit, l_idStrongs, l_def in l_cursor:
+
+            # diacritics removal
             l_firstGround = g_greekLetterTranslate[l_ground[0]]
+
             if l_curInitial != l_firstGround:
                 if l_curInitial != '':
                     l_response += '</table><br/>'
 
                 g_loggerLexicon.debug('l_letterCount {0}'.format(l_letterCount))
                 if l_letterCount == 12:
-                    for i in range(166):
-                        l_response += '<p>&nbsp;</p>\n'
-                    l_response += '</div><div class="LexRight">\n'
+                    l_response += '</td><td class="LexRight">\n'
 
                 l_letterCount += 1
 
@@ -269,19 +290,21 @@ def lexicon_greek(p_previousContext, p_context, p_dbConnectionPool):
                               '<tr><td colspan="2" class="LexLetter">{0}</td></tr>\n'.format(l_firstGround)
                 l_curInitial = l_firstGround
 
-            l_rootLink = '<a href="" class="GoWord" wordid="_-_-{1}">{0}</a>'.format(l_ground, l_idStrongs)
+            l_wordLink = '<a href="" class="GoWord" wordid="_-_-{1}">{0}</a>'.format(l_ground, l_idStrongs)
 
-            l_response += '<tr><td class="LexGround">{0}</td>\n'.format(l_rootLink)
+            # first cell --> word
+            l_response += '<tr><td class="LexGround">{0}</td>\n'.format(l_wordLink)
+            # second cell --> transliteration
             l_response += '<td class="LexTranslit">{0}</td>\n'.format(l_translit)
+            # third cell --> translation
             l_response += '<td class="LexDef">{0}</td></tr>\n'.format(l_def)
 
         l_cursor.close()
     except Exception as l_exception:
         g_loggerLexicon.warning('Something went wrong {0}'.format(l_exception.args))
 
-    l_response += '</table></div></div>\n'
+    l_response += '</table></td></tr></table></div>\n'
 
     p_dbConnectionPool.releaseConnection(l_dbConnection)
 
     return l_response, p_context, ec_app_params.g_appTitle
-
